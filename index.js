360f830e08b656b7dd8b8cd7f1d5b385a31a0c7b
Kb8lightly <108156900+Kb8lightly@users.noreply.github.com>
2024-03-04 18:13:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const MONGODB_URI = process.env['Mongodb_uri'];
+const MONGODB_URI = process.env['Mongodburi'];
 
 // Middleware
 app.use(bodyParser.json());
